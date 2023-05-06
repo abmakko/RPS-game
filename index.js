6630d1a9f134d6ea45668e9b6f8169eff06eb103
abmakko <36611833+abmakko@users.noreply.github.com>
2023-05-06 16:19:36 +0300

    finished gameplay ui flow

--- index.js
+++ index.js
@@ -9,34 +9,174 @@ let draw_counter = 0;
 
 let messages = '';
 
-let rock_event = document.getElementById("rock");
-let paper_event = document.getElementById("paper");
-let scissor_event = document.getElementById("scissor");
+const rock_event = document.getElementById("rock");
+const paper_event = document.getElementById("paper");
+const scissor_event = document.getElementById("scissor");
+
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
 
 rock_event.addEventListener('click', init_animation);
 paper_event.addEventListener('click', init_animation);
 scissor_event.addEventListener('click', init_animation);
 
-let count_container = document.getElementById("message");
-let counter = document.getElementsByClassName("slides");
-let rock_rightSide = document.getElementById("rock-right");
-let rock_leftSide = document.getElementById("rock-left");
+const count_container = document.getElementById("message");
+const counter = document.getElementsByClassName("slides");
+
+const rock_rightSide = document.getElementById("rock-right");
+const rock_leftSide = document.getElementById("rock-left");
+
+const scissor_leftSide = document.getElementById("scissor-left");
+const scissor_rightSide = document.getElementById("scissor-right");
+
+const paper_leftSide = document.getElementById("paper-left");
+const paper_rightSide = document.getElementById("paper-right");
+
+const button_group = document.getElementsByClassName("action-btn");
+
+const hand_display = document.getElementsByClassName("hand-container");
+
+const tags = document.getElementsByClassName("tag");
+
+const prompt_msg = document.getElementById("prompt-text");
+
+rock_rightSide.addEventListener('animationend', gameState_1);
+
+let computerChoice = Math.floor(Math.random() * 3) + 1;
+
+function gameState_1(){
+    playerScore.textContent = player_win_tally;
+    computerScore.textContent = computer_win_tally;
+    prompt_msg.textContent = messages;
+    console.log(messages);    
+    Array.from(button_group).forEach(element => element.disabled = false);  
+    Array.from(counter).forEach(element => element.classList.remove("number")); 
+    rock_leftSide.classList.remove("animate_2");
+    rock_rightSide.classList.remove("animate_1");
+    count_container.style.display = "none";
+
+
+    switch(player_option){
+        case 1:
+            rock_leftSide.style.display = "flex";
+            scissor_leftSide.style.display = "none";
+            paper_leftSide.style.display = "none";            
+            break;
+        case 2:
+            paper_leftSide.style.display = "flex";
+            rock_leftSide.style.display = "none";
+            scissor_leftSide.style.display = "none";            
+            break;
+        case 3:
+            scissor_leftSide.style.display = "flex";
+            rock_leftSide.style.display = "none";        
+            paper_leftSide.style.display = "none";            
+            break;
+    }
+
+    switch(computerChoice){
+        case 1:
+            rock_rightSide.style.display = "flex";
+            scissor_rightSide.style.display = "none";
+            paper_rightSide.style.display = "none";
+            
+            break;
+        case 2:
+            paper_rightSide.style.display = "flex";
+            rock_rightSide.style.display = "none";
+            scissor_rightSide.style.display = "none";            
+            break;
+        case 3:
+            scissor_rightSide.style.display = "flex";
+            rock_rightSide.style.display = "none";        
+            paper_rightSide.style.display = "none";            
+            break;
+    }   
+}
 
 function anim_1(){ 
     count_container.style.display = "flex";
     Array.from(counter).forEach(element => element.classList.add("number"));
     rock_leftSide.style.display = "flex";
     rock_rightSide.style.display = "flex";
-    rock_leftSide.className += " animate_2";
-    rock_rightSide.className += " animate_1";
+    paper_leftSide.style.display = "none";
+    paper_rightSide.style.display = "none";
+    scissor_leftSide.style.display = "none";
+    scissor_rightSide.style.display = "none";
+    rock_leftSide.classList.add("animate_2");
+    rock_rightSide.classList.add("animate_1");
 }
 
-function init_animation(){
-    console.log('button pressed');
+function init_animation(e){
+    prompt_msg.innerHTML = '';
+    computerChoice = Math.floor(Math.random() * 3) + 1;
+    Array.from(button_group).forEach(element => element.disabled = true);
+    Array.from(tags).forEach(element => element.style.display = 'none');    
+    
     anim_1();
+    switch(e.target.id){
+        case 'rock': 
+            player_option = 1;
+            if(computerChoice === 1 && player_option === 1){
+            computer_option = "rock";                           
+            messages = "DRAW";       
+            
+        }
+        else if (computerChoice === 2){
+            computer_option = "paper";                       
+            computer_win_tally++;
+            messages = "YOU LOST";  
+        }
+        else if(computerChoice === 3){
+            computer_option = "scissor"; 
+            messages = "YOU WON";            
+            player_win_tally++;
+        }
+
+        
+
+        break;
+
+        case 'paper':
+            player_option = 2;
+            if(computerChoice === 2 && player_option === 2){
+                computer_option = "paper";
+                messages = "DRAW"; 
+            }
+            else if(computerChoice === 1){
+                computer_option = "rock";
+                player_win_tally++;
+                messages = "YOU WON"; 
+            }
+            else if(computerChoice === 3){
+                computer_option = "scissor";
+                computer_win_tally++
+                messages = "YOU LOST"; 
+            }
+
+            break;
+
+            case 'scissor':
+                player_option = 3;
+                if(computerChoice === 3 && player_option === 3){
+                    computer_option = "scissor";
+                    messages = "DRAW"; 
+                }
+                else if (computerChoice === 1){
+                    computer_option = "rock";
+                    computer_win_tally++;
+                    messages = "YOU LOST"; 
+                }
+                else if(computerChoice === 2){
+                    computer_option = "paper";
+                    player_win_tally++;
+                    messages = "YOU WON"; 
+                }
+                break;
+    }
 }
 
-let computerChoice = Math.floor(Math.random() * 3) + 1;
+
 
 
 function game_logic(){    
